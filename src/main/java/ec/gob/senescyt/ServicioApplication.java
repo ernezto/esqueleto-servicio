83127c4f1d029c50f6b8531c2f6ac649a08daa54
diablouma <mh732@hotmail.com>
2014-09-09 14:39:12 -0500

    [#0] - @diablouma - se actualiza aplicacion y configuracion para usar microservicio-commons

--- src/main/java/ec/gob/senescyt/ServicioApplication.java
+++ src/main/java/ec/gob/senescyt/ServicioApplication.java
@@ -5,38 +5,31 @@ import com.sun.jersey.api.core.ResourceConfig;
 import ec.gob.senescyt.commons.bundles.DBMigrationsBundle;
 import ec.gob.senescyt.commons.exceptions.DBConstraintViolationMapper;
 import ec.gob.senescyt.commons.exceptions.ValidacionExceptionMapper;
-import ec.gob.senescyt.commons.filters.HeaderResponseFilter;
-import ec.gob.senescyt.commons.filters.RedirectFilter;
 import ec.gob.senescyt.ejemplo.core.Ejemplo;
 import ec.gob.senescyt.ejemplo.resources.EjemploResource;
-import io.dropwizard.Application;
+import ec.gob.senescyt.microservicios.commons.MicroservicioAplicacion;
+import ec.gob.senescyt.microservicios.commons.MicroservicioConfiguracion;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.hibernate.SessionFactory;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
 import javax.ws.rs.ext.ExceptionMapper;
-import java.nio.charset.StandardCharsets;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class ServicioApplication extends Application<ServicioConfiguration> {
+public class ServicioApplication extends MicroservicioAplicacion<ServicioConfiguration> {
 
     private final DBMigrationsBundle flywayBundle = new DBMigrationsBundle();
-    private final HibernateBundle<ServicioConfiguration> hibernate = new HibernateBundle<ServicioConfiguration>(Ejemplo.class) {
+    private final HibernateBundle<MicroservicioConfiguracion> hibernate = new HibernateBundle<MicroservicioConfiguracion>(Ejemplo.class) {
         @Override
-        public DataSourceFactory getDataSourceFactory(ServicioConfiguration configuration) {
+        public DataSourceFactory getDataSourceFactory(MicroservicioConfiguracion configuration) {
             return configuration.getDataSourceFactory();
         }
     };
-    private ServicioConfiguration configuration;
 
     public static void main(String[] args) throws Exception {
         new ServicioApplication().run(args);
@@ -48,37 +41,29 @@ public class ServicioApplication extends Application<ServicioConfiguration> {
     }
 
     @Override
-    public void initialize(Bootstrap<ServicioConfiguration> bootstrap) {
+    public void inicializar(Bootstrap<ServicioConfiguration> bootstrap) {
         bootstrap.addBundle(hibernate);
         bootstrap.addBundle(flywayBundle);
     }
 
     @Override
-    public void run(ServicioConfiguration configuration, Environment environment) throws Exception {
-        this.configuration = configuration;
+    public void ejecutar(ServicioConfiguration servicioConfiguration, Environment environment) {
         JerseyEnvironment jerseyEnvironment = environment.jersey();
 
         EjemploResource ejemploResource = new EjemploResource();
         jerseyEnvironment.register(ejemploResource);
 
-        registrarFiltros(environment);
         registrarValidacionExceptionMapper(environment);
+    }
+
+    @Override
+    protected HibernateBundle<MicroservicioConfiguracion> getHibernate() {
+        return hibernate;
     }
 
     @VisibleForTesting
     public SessionFactory getSessionFactory() {
         return hibernate.getSessionFactory();
-    }
-
-    private void registrarFiltros(Environment environment) {
-        environment.jersey().getResourceConfig().getContainerResponseFilters().add(new HeaderResponseFilter(StandardCharsets.UTF_8.name()));
-
-        environment.servlets().addFilter("cors-filter", CrossOriginFilter.class)
-                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-
-        FilterRegistration.Dynamic filtro = environment.servlets().addFilter("redirect-filter", RedirectFilter.class);
-        filtro.setInitParameter("httpsPort", configuration.getHttpsPort());
-        filtro.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
     }
 
     private void registrarValidacionExceptionMapper(Environment environment) {
--- src/main/java/ec/gob/senescyt/ServicioConfiguration.java
+++ src/main/java/ec/gob/senescyt/ServicioConfiguration.java
@@ -1,15 +1,13 @@
 package ec.gob.senescyt;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.dropwizard.Configuration;
+import ec.gob.senescyt.microservicios.commons.MicroservicioConfiguracion;
 import io.dropwizard.db.DataSourceFactory;
-import io.dropwizard.jetty.HttpConnectorFactory;
-import io.dropwizard.server.DefaultServerFactory;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-public class ServicioConfiguration extends Configuration {
+public class ServicioConfiguration extends MicroservicioConfiguracion {
 
     @Valid
     @NotNull
@@ -22,12 +20,5 @@ public class ServicioConfiguration extends Configuration {
 
     public String getDefaultSchema() {
         return database.getProperties().get("hibernate.default_schema");
-    }
-
-    public String getHttpsPort() {
-        DefaultServerFactory serverFactory = (DefaultServerFactory) getServerFactory();
-        HttpConnectorFactory httpConnectorFactory = (HttpConnectorFactory) serverFactory.getApplicationConnectors().get(1);
-
-        return String.valueOf(httpConnectorFactory.getPort());
     }
 }
